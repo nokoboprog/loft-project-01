285c969e281728ec1c4bd65ea9622d1c22b8bfa0
Kurmanbek Nurmatov <nokobo.prog@gmail.com>
2020-03-11 13:28:05 +0300

    Task 5.2 done

--- admin.php
+++ admin.php
@@ -2,25 +2,17 @@
 
 require_once('config.php');
 
-//Cписок всех зарегистрированных пользователей.
-echo '<b style="color: brown">Список всех пользователей:</b><br><br>';
-echo '<table style="border: 1px solid #000">';
-$th = "<th style='border: 1px solid #000'>";
-$td = "<td style='border: 1px solid #000'>";
-echo "<tr>{$th}№</th>{$th}Имя</th>{$th}Email</th>{$th}Телефон</th></tr>";
 $query = $pdo->query('SELECT * FROM users');
+$dataUsers = [];
 while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-    echo "<tr>{$td}{$row->id}</td>{$td}$row->name</td>{$td}$row->email</td>{$td}$row->phone</td></tr>";
+    $dataUsers[] = $row;
 }
-echo '</table><br><br>';
 
-//Cписок всех заказов.
-echo '<b style="color: brown">Список всех заказов:</b><br><br>';
-echo '<table style="border: 1px solid #000">';
-echo "<tr>{$th}№</th>{$th}Имя</th>{$th}Email</th>{$th}Телефон</th>";
-echo "{$th}Адрес</th>{$th}Комментарий</th>{$th}Потребуется сдача/<br>Оплата по карте</th>{$th}Перезвонить</th>";
 $query = $pdo->query('SELECT orders.*, users.email, users.name, users.phone FROM orders LEFT JOIN users ON users.id = orders.user_id GROUP BY orders.id');
+$dataOrders = [];
 while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-    echo "<tr>{$td}{$row->id}</td>{$td}$row->name</td>{$td}$row->email</td>{$td}$row->phone</td>{$td}$row->address</td>{$td}$row->comment</td>{$td}$row->payment</td>{$td}$row->callback</td></tr>";
+    $dataOrders[] = $row;
 }
-echo '</table>';
+
+$template = $twig->load('admin.twig');
+echo $template->render(['users' => $dataUsers, 'orders' => $dataOrders]);
--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader('templates');
+$twig = new Environment($loader);
+
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=db_burgers', 'root', '');
 } catch (PDOException $e) {
